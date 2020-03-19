4588c0c9d47a4a26977eb11d1a5daa1b5c130a32
Wisdom Ekpot <wisdomekpot@gmail.com>
2020-03-19 21:50:26 +0100

    litle applicaton with vuejs

---
+++ app.js
@@ -0,0 +1,57 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    message: 'Hello Vue!',
+    vueCanvas:null,
+    painting:false,
+    canvas:null,
+    ctx:null
+  },
+  methods: {
+    startPainting(e) {
+      this.painting = true;
+      console.log(this.painting)
+      this.draw(e)
+    },
+    finishedPainting() {
+      this.painting = false;
+      console.log(this.painting);
+      this.ctx.beginPath()
+    },
+    draw(e) {
+      if(!this.painting) return
+
+      this.ctx.lineWidth = 10;
+      this.ctx.lineCap ="round"
+      
+     this.ctx.lineTo(e.clientX,e.clientY)
+     this.ctx.stroke()
+
+     this.ctx.beginPath()
+     this.ctx.moveTo(e.clientX,e.clientY)
+
+     
+    }
+  },
+ mounted() {
+  this.canvas = document.getElementById("canvas");
+  this.ctx = canvas.getContext("2d");  
+
+  // Resize canvas
+  this.canvas.height = window.innerHeight;
+  this.canvas.width = window.innerWidth;
+
+
+  //draw a simple triange
+    // ctx.strokeStyle = "green"
+    // ctx.strokeRect(100,100,300,300)
+
+    //draw a simple traingle
+  //  ctx.beginPath();
+  //  ctx.moveTo(100,100)
+  //  ctx.lineTo(200,100)
+  //  ctx.lineTo(200,150)
+  //  ctx.closePath()
+  //  ctx.stroke()
+}
+})
